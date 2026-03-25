62f44df86dff2bcd109ab8384b0af1fc52c95b9e
Ayden Serna <114900435+nmsjayden@users.noreply.github.com>
2026-03-25 14:26:15 -0500

    Create index.js

---
+++ get_key/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script>
+    const query = window.location.search;
+
+    sessionStorage.setItem("get_key_query", query);
+
+    window.location.replace("/luarmor/");
+  </script>
+</head>
+<body></body>
+</html>
