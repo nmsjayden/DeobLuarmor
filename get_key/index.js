be46c1701f831f7c7fd2f5a87881da855f908c70
Ayden Serna <114900435+nmsjayden@users.noreply.github.com>
2026-03-25 18:47:47 -0500

    Update index.js

--- get_key/index.js
+++ get_key/index.js
@@ -2,13 +2,24 @@
 <html>
 <head>
   <script>
-    const query = window.location.search;
+(function () {
+  const DEFAULT_KEY = "-dsYICZJAvKVC";
 
-    sessionStorage.setItem("get_key_query", query);
-    const BASE_PATH = "/" + window.location.pathname.split("/")[1];
+  // Detect current folder (base path)
+  const basePath = "/" + window.location.pathname.split("/")[1];
 
-    window.location.replace(BASE_PATH + "/");
-  </script>
+  const params = new URLSearchParams(window.location.search);
+  const hasKey = params.has("for");
+
+  const isGetKeyPage = window.location.pathname.endsWith("/get_key") || window.location.pathname.includes("/get_key/");
+
+  // Only redirect if we're NOT already on /get_key with ?for
+  if (!isGetKeyPage || !hasKey) {
+    const newUrl = basePath + "/get_key/?for=" + DEFAULT_KEY;
+    history.replaceState({}, "", newUrl);
+  }
+})();
+</script>
 </head>
 <body></body>
 </html>
